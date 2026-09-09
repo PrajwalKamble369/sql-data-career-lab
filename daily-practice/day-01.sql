44342e9357f68ed5c7bdbf0574de8bf2893934ed
Prajwal Kamble <prajwal17022000@gmail.com>
2026-09-09 15:04:58 +0530

    question  2 solved

--- daily-practice/day-01.sql
+++ daily-practice/day-01.sql
@@ -31,3 +31,34 @@ ORDER BY
     customer_id ASC
 LIMIT 20
 ;
+
+/*
+Maharashtra Customers
+
+Using the customers table, return:
+
+customer_id
+first_name
+last_name
+city
+customer_segment
+
+Requirements
+Return only customers from Maharashtra.
+Return the first 25 customers.
+Sort by customer_id in ascending order.
+Do not use SELECT
+
+*/
+
+SELECT
+    customer_id, 
+    first_name, 
+    last_name, 
+    city,customer_segment
+FROM 
+    customers
+WHERE state = 'Maharashtra'
+ORDER BY customer_id ASC
+LIMIT 25;
+
