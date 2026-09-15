defcd9a3fca56c7e7aeaf01a152877d06e765a66
Prajwal Kamble <prajwal17022000@gmail.com>
2026-09-15 12:54:27 +0530

    q2 solved

--- daily-practice/day-05.sql
+++ daily-practice/day-05.sql
@@ -40,3 +40,50 @@ HAVING
 ORDER BY
     number_of_orders DESC,
     c.customer_id ASC;
+
+/*
+
+2. Delivered Revenue by Customer
+
+Using customers and orders, calculate for each customer:
+
+customer_id
+first_name
+last_name
+total delivered revenue
+average delivered order value
+
+Only consider delivered orders.
+
+Only show customers whose total delivered revenue is at least ₹50,000.
+
+Sort by total delivered revenue descending.
+
+Focus: JOIN, WHERE, SUM, AVG, GROUP BY, HAVING
+
+*/
+
+
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    SUM(o.total_amount) AS total_delivered_revenue,
+    AVG(o.total_amount) AS avg_delivered_order_value
+FROM
+    customers c
+JOIN
+    orders o
+ON
+    c.customer_id = o.customer_id
+WHERE
+    o.order_status = 'delivered'
+GROUP BY
+    c.customer_id,
+    c.first_name,
+    c.last_name
+HAVING
+    SUM(o.total_amount) >= 50000
+ORDER BY
+    total_delivered_revenue DESC;
