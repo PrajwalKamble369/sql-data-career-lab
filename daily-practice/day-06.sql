e94228108da9b7792873c9b228ec5144a16a57e2
Prajwal Kamble <prajwal17022000@gmail.com>
2026-09-21 13:24:19 +0530

    q2 solved

--- daily-practice/day-06.sql
+++ daily-practice/day-06.sql
@@ -22,14 +22,80 @@ Sort by total_delivered_revenue descending.
 
 */
 
-SELECT * FROM customers;
-SELECT * FROM orders;
 
 SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    SUM(o.total_amount) AS total_delivered_revenue,
+    CASE
+        WHEN SUM(o.total_amount) >=200000 THEN 'High_Value'
+        WHEN SUM(o.total_amount) >=100000 THEN  'Medium_Value'
+        ELSE 'Low_Value'
+    END AS spending_tier
+FROM
+    customers c
+JOIN
+    orders o
+ON
+    c.customer_id = o.customer_id
+WHERE
+    o.order_status = 'delivered'
+GROUP BY
+    c.customer_id,
+    c.first_name,
+    c.last_name
+ORDER BY
+    total_delivered_revenue DESC;
+
+
+/*
+
+2. Order Value Classification
+
+Difficulty: 🟢 → 🟡
+Focus: CASE · NULL handling · filtering · sorting
+
+Using orders, return:
+
+order_id
+customer_id
+total_amount
+order_status
+order_value_category
+
+Classification:
+
+>= 50000 → Very High
+>= 20000 → High
+>= 5000 → Medium
+< 5000 → Low
+NULL → Unknown
+
+Only include orders with status delivered or shipped.
+
+Sort by total_amount descending, with NULL values last.
+
+Return the top 30.
+
+*/
+
+SELECT
+    order_id,
     customer_id,
-    first_name,
-    last_name,
-    SUM(total_amount) ,
-    (CASE WHEN SUM(total_amount >=200000) THEN 1 END) AS High_Value,
-    (CASE WHEN SUM(total_amount >=100000) THEN 1 END) AS Medium_Value,
-    (CASE WHEN SUM(total_amount <100000) THEN 1 END) AS Low_Value
+    total_amount,
+    order_status,
+    CASE
+        WHEN total_amount >= 50000 THEN 'Very High'
+        WHEN total_amount >= 20000 THEN 'High'
+        WHEN total_amount >= 5000 THEN 'Medium'
+        WHEN total_amount < 5000 THEN 'LOW'
+        ELSE 'UNKOWN'
+    END AS order_value_category
+FROM
+    orders
+WHERE
+    order_status IN ('delivered', 'shipped')
+ORDER BY
+    total_amount DESC NULLS LAST
+LIMIT 30;
