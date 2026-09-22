80e47c07a4068237cf7334470bfceb35960762d4
Prajwal Kamble <prajwal17022000@gmail.com>
2026-09-22 12:46:50 +0530

    q3 solved

--- daily-practice/day-07.sql
+++ daily-practice/day-07.sql
@@ -91,3 +91,63 @@ WHERE
             products)
 ORDER BY
     price DESC;
+
+/*
+
+3. Customers With Above-Average Order Value
+
+Difficulty: 🟡 → 🔴
+Focus: subquery + aggregation
+
+Find customers whose average order value is greater than the overall average order value across all orders.
+
+Return:
+
+customer_id
+first_name
+last_name
+order_count
+avg_order_value
+
+Requirements:
+
+Consider all orders.
+Customers must have at least 3 orders.
+Sort by avg_order_value descending.
+
+Think carefully about the difference between:
+    Average order value per customer
+
+and
+
+    Average order value across the entire orders table
+*/
+
+
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    COUNT(o.order_id) AS order_count,
+    AVG(o.total_amount) AS avg_order_value
+FROM
+    customers c
+JOIN
+    orders o
+ON
+    c.customer_id = o.customer_id
+GROUP BY
+    c.customer_id,
+    c.first_name,
+    c.last_name
+HAVING
+    COUNT(o.order_id) >=3
+    AND
+    AVG(o.total_amount) >
+        (SELECT
+            AVG(total_amount) 
+        FROM 
+            orders)
+ORDER BY
+    avg_order_value DESC;
